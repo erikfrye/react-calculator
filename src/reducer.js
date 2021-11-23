2a7ddf8f8b23fbfe359fef682b57bfe9b780cd46
Erik Frye <erikfrye@gmail.com>
2021-11-23 16:27:25 -0500

    fix first character decimal

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,12 @@
 import { evaluate } from "./evaluate";
 
 export const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate'
-}
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
 
 export function reducer(state, { type, payload }) {
   switch (type) {
@@ -16,76 +16,86 @@ export function reducer(state, { type, payload }) {
           ...state,
           currentOperand: payload.digit,
           overwrite: false,
-        }
+        };
 
-      if (payload.digit === "0" && state.currentOperand === "0") 
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+
+      if (payload.digit === "." && state.currentOperand == null)
+        return {
+          ...state,
+          currentOperand: `0${payload.digit}`,
+        };
+
+      if (payload.digit === "." && state.currentOperand.includes("."))
         return state;
 
-      if (payload.digit === "." && state.currentOperand.includes(".")) 
-        return state;
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      }
+      };
 
     case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null) 
+      if (state.currentOperand == null && state.previousOperand == null)
         return state;
 
       if (state.currentOperand == null) {
         return {
           ...state,
-          operation: payload.operation
-        }
+          operation: payload.operation,
+        };
       }
-      
+
       if (state.previousOperand == null) {
         return {
           ...state,
           operation: payload.operation,
           previousOperand: state.currentOperand,
           currentOperand: null,
-        }
+        };
       }
       return {
         ...state,
         previousOperand: evaluate(state),
         operation: payload.operation,
-        currentOperand: null
-      }
+        currentOperand: null,
+      };
 
     case ACTIONS.CLEAR:
-      return {}
+      return {};
 
     case ACTIONS.DELETE_DIGIT:
       if (state.overwrite) {
         return {
           ...state,
-          overwrite:false,
-          currentOperand: null
-        }
-      }  
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
       if (state.currentOperand == null) return state;
-      if (state.currentOperand.length === 1) 
-        return { ...state, currentOperand: null }
-    
+      if (state.currentOperand.length === 1)
+        return { ...state, currentOperand: null };
+
       return {
         ...state,
-        currentOperand: state.currentOperand.slice(0,-1),
-      }
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
 
     case ACTIONS.EVALUATE:
-      if (state.operation == null || state.currentOperand == null || state.previousOperand == null )
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      )
         return state;
-      
+
       return {
         ...state,
         overwrite: true,
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
-      }
-      
+      };
+
     default:
       return state;
   }
